d9fc63500364ed0df27c612803f9248a9cfdb74a
Moneer Almawaldi <22almawm@elmbrookstudents.org>
2018-09-13 13:17:45 -0500

    NameageProgram

---
+++ src/outputExamples/AgenameProgram.java
@@ -0,0 +1,13 @@
+package outputExamples;
+
+public class AgenameProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("First Name: Moneer");
+	System.out.println("Last Name: Almawaldi");
+    System.out.println("Age: 14");
+
+	}
+
+}
--- src/outputExamples/NameProgram.java
+++ src/outputExamples/NameProgram.java
@@ -5,7 +5,7 @@ public class NameProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Moneer");
-System.out.print("Almawaldi");
+System.out.println("Almawaldi");
 
 	}
 
