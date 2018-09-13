6f77cae426f8d8db50498bb8012a91eee9d04205
Moneer Almawaldi <22almawm@elmbrookstudents.org>
2018-09-13 13:10:12 -0500

    NameProgram

---
+++ src/outputExamples/NameProgram.java
@@ -0,0 +1,12 @@
+package outputExamples;
+
+public class NameProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Moneer");
+System.out.print("Almawaldi");
+
+	}
+
+}
