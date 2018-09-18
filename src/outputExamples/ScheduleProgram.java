6885683471bbc0a2a71cc88eacaae77d86fa759b
Moneer Almawaldi <22almawm@elmbrookstudents.org>
2018-09-18 13:41:52 -0500

    ScheduleProgram

--- src/outputExamples/ScheduleProgram.java
+++ src/outputExamples/ScheduleProgram.java
@@ -5,9 +5,9 @@ public class ScheduleProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.println("Moneer Almawaldi 2018-2019 Schedule");
-    System.out.println("        Q1            Q2              Q3             Q4");
-    System.out.println("Block 1: World History World History  Honors Geometry Honors Geometry ");
-	
+    System.out.println("        Q1             Q2              Q3             Q4");
+    System.out.println("Block 1: World History  World History  Honors Geometry Honors Geometry ");
+	System.out.println("Block 2: Honors English Honors English Physical Ed.   Study Hall");
 
 	}
 
