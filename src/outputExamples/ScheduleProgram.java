91bbe0e7b833381162e697eed4507c10f4e13ac3
Moneer Almawaldi <22almawm@elmbrookstudents.org>
2018-09-19 14:53:30 -0500

    inputExamples

--- src/outputExamples/ScheduleProgram.java
+++ src/outputExamples/ScheduleProgram.java
@@ -8,7 +8,11 @@ public class ScheduleProgram {
     System.out.println("        Q1             Q2              Q3             Q4");
     System.out.println("Block 1: World History  World History  Honors Geometry Honors Geometry ");
 	System.out.println("Block 2: Honors English Honors English Physical Ed.   Study Hall");
+    int age=17;
+    System.out.println(age);
+    
 
+    		
 	}
 
 }
