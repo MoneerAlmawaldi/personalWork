dd0d174b04a25c2650bd3dd0664100973497ea24
Moneer Almawaldi <22almawm@elmbrookstudents.org>
2018-09-20 13:12:38 -0500

    VotingAnswer

---
+++ src/inputExamples/Monkey.java
@@ -0,0 +1,19 @@
+package inputExamples;
+
+import java.util.Scanner;
+
+public class Monkey {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double money=-999;
+Scanner input= new Scanner(System.in);
+System.out.println("How much money is in your pocket?"+money);
+money=input.nextDouble();
+
+
+
+
+	}
+
+}
