bab0cd0e97b71948a691e1d2ce78aa92fa75880b
Moneer Almawaldi <22almawm@elmbrookstudents.org>
2018-09-20 13:33:26 -0500

    PracticeFail

---
+++ src/inputExamples/AgeNameProgramTwo.java
@@ -0,0 +1,21 @@
+package inputExamples;
+
+import java.util.Scanner;
+
+public class AgeNameProgramTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input= new Scanner(System.in);
+		int age=14;
+		age=input.nextInt();
+		System.out.println("What is your age?");
+		System.out.println(age);
+		double weight=111.1;
+		weight=input.nextDouble();
+		System.out.println("What is your weight?");
+        System.out.println(weight);
+
+	}
+
+}
