1a9e7d80a767d353f98b74926d086c11ae845ff5
Moneer Almawaldi <22almawm@elmbrookstudents.org>
2018-09-21 14:28:07 -0500

    FutureAge

---
+++ src/inputExamples/FutureAge.java
@@ -0,0 +1,20 @@
+package inputExamples;
+
+import java.util.Scanner;
+
+public class FutureAge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age= -999;
+		Scanner input= new Scanner(System.in);
+		System.out.println("Current Age: "+age);
+		age=age+7;
+		System.out.println("Age In Seven Years: "+age);
+		age=input.nextInt();
+		
+
+
+	}
+
+}
