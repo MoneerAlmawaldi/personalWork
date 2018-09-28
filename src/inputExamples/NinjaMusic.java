ddd6399ce57362291680a4e1d4402f9d280cb27b
Moneer Almawaldi <22almawm@elmbrookstudents.org>
2018-09-28 14:43:36 -0500

    NinjaMusic

---
+++ src/inputExamples/NinjaMusic.java
@@ -0,0 +1,26 @@
+package inputExamples;
+import java.util.Scanner;
+public class NinjaMusic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double decimal=-999;
+decimal= 34/60;
+double Song1= 2.56666667;
+double Song2= 2.08333333;
+double Song3= 2.78333333;
+double Song4= 2.0;
+double average=-999;
+System.out.println("What Is The Average Songlength?");
+average= (Song1+Song2+Song3+Song4)/4;
+decimal=0.35*60;
+System.out.println("The Average Songlength Is: "+'2'(decimal));
+
+
+
+
+
+
+	}
+
+}
