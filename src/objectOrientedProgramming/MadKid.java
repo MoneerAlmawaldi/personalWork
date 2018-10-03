0f69ef0aa80cd1e038b1cc70b3c7a1417ea15510
Moneer Almawaldi <22almawm@elmbrookstudents.org>
2018-10-03 13:07:14 -0500

    PPracticePracticeOOP

---
+++ src/objectOrientedProgramming/MadKid.java
@@ -0,0 +1,21 @@
+package objectOrientedProgramming;
+
+public class MadKid {
+
+	public static void main(String[] args) {
+	} 
+		// TODO Auto-generated method stub
+			public MadKid(){
+
+
+			}
+			public Object sayMad;
+			public void sayMad(){
+			System.out.println("I am so Angry!!!");}
+			
+			public Object sayHate;
+			public void sayHate(){
+			System.out.println("I hate you!!");
+	}
+
+}
