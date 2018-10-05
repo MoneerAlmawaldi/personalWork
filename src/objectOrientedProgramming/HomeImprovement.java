85549f6e80a280f99357bdc2f9c4dc202719186c
Moneer Almawaldi <22almawm@elmbrookstudents.org>
2018-10-05 13:54:23 -0500

    TestPracticeOOP

--- src/objectOrientedProgramming/HomeImprovement.java
+++ src/objectOrientedProgramming/HomeImprovement.java
@@ -11,8 +11,25 @@ int length=input.nextInt();
 System.out.println("Height= ");
 int height= input.nextInt();
 int area= length*height;
+double hours= area/3.33333333;
+System.out.println("Total Minutes Required To Paint The Wall: "+hours);
+}
+public void gardenHours(){
+Scanner input=new Scanner(System.in);
+final int WIDTH=10;
+System.out.println("What Is The Length Of The Garden?");
+System.out.println("Length: ");
+int length=input.nextInt();
+System.out.println("How Many Rows Are There In The Garden?");
+System.out.println("Rows= ");
+int rows=input.nextInt();
+int totallength=length*rows;
+int gardentime=totallength/15;
+System.out.println("Total Hours It Takes To Plant The Garden: "+gardentime*2);
 
+
+
+}
 
 	}
 
-}
---
+++ src/objectOrientedProgramming/TestClass.java
@@ -0,0 +1,14 @@
+package objectOrientedProgramming;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+HomeImprovement bob=new HomeImprovement();
+bob.wallArea();
+HomeImprovement john=new HomeImprovement();
+john.gardenHours();
+
+	}
+
+}
