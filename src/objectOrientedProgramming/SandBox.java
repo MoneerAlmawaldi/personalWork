df7ce68595f6b5ff0cb0e562e163e44eae0e4f7a
Moneer Almawaldi <22almawm@elmbrookstudents.org>
2018-10-08 21:55:03 -0500

    SandBox

---
+++ src/objectOrientedProgramming/SandBox.java
@@ -0,0 +1,9 @@
+package objectOrientedProgramming;
+import java.util.Scanner;
+public class SandBox {
+public SandBox() {
+	}
+public void sayHours(double length, double width, double depth){
+System.out.println("How Much Hours It Takes To Fill The Sandbox: ");
+}
+}
---
+++ src/objectOrientedProgramming/SandBoxTest.java
@@ -0,0 +1,9 @@
+package objectOrientedProgramming;
+
+public class SandBoxTest {
+public static void main(String[] args) {
+SandBox phil=new SandBox();
+phil.sayHours=(1.0);
+}
+}
+
