a30df59afb2c0052703d33854828f3a26d8a51a3
Moneer Almawaldi <22almawm@elmbrookstudents.org>
2018-10-16 15:07:28 -0500

    Unitthreebeginning

---
+++ src/decisionStructures/Shoe.java
@@ -0,0 +1,13 @@
+package decisionStructures;
+
+import java.util.Scanner;
+
+public class Shoe {
+Scanner input=new Scanner(System.in);
+System.out.println("Enter height");
+double height=input.nextDouble();
+System.out.println("Shoe Size");
+double size=input.nextDouble();
+
+
+}
