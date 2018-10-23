a98ee95b57b9b6660f1005368e025aabe9e1aa18
Moneer Almawaldi <22almawm@elmbrookstudents.org>
2018-10-23 15:08:56 -0500

    WhileLoops

---
+++ src/Loops/Activity6.java
@@ -0,0 +1,18 @@
+package Loops;
+import java.util.Scanner;
+public class Activity6 {
+int number=0;
+int counter=0;
+int average=0;
+Scanner input=new Scanner(System.in);
+System.out.println("Enter A Number. (999 To Quit)");
+number=input.nextInt();
+while(number!=999)sssssss
+{
+average=number*8;
+System.out.println("Enter A Number. (999 To Quit)");
+counter++;
+number=input.nextInt();
+}
+System.out.println("The Average Of Numbers Entered Is: "+average);
+
