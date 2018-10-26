4de3d43cfb6dde90546ba50d3802f21d46b5c107
Moneer Almawaldi <22almawm@elmbrookstudents.org>
2018-10-26 11:05:24 -0500

    LoopsAndStuff

--- src/Loops/Activity1.java
+++ src/Loops/Activity1.java
@@ -1,16 +1,17 @@
 package Loops;
 import java.util.Scanner;
 public class Activity1 {
+public static void main(String[]args){
 Scanner input=new Scanner(System.in);
 int largest=0;
 System.out.println("Enter Number.");
-int number=input.nextInt();
 do 
 {
+int number=0;
 if (number>largest)
 largest=number;
 System.out.println("Enter a number(9999 to quit):");
-int number=input.nextInt();
+number=input.nextInt();
 }while(number!=9999);
 System.out.println("The Largest Number Is: ");
 	
--- src/Loops/ForLoops.java
+++ src/Loops/ForLoops.java
@@ -4,10 +4,14 @@ public class ForLoops {
 public static void main(String[] args){
 Scanner input=new Scanner(System.in);
 int counter=0;
-int howmany=input.nextInt();
-for(counter=1;counter<=10;counter=counter+1)
+int total=0;
+int first=input.nextInt();
+for(counter=1;counter<=5;counter=counter+1)
 {
-System.out.println(howmany+ " Hello");
+System.out.println("Enter a number "+ counter);
+first=input.nextInt();
+total+=first;
 }
+System.out.println(total);
 }
 }
