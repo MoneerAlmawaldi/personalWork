6d80c2cdf2b144c6b46a0b33a6db0717c0367fc5
Moneer Almawaldi <22almawm@elmbrookstudents.org>
2018-10-30 15:08:17 -0500

    ActuallyLearningLoops

--- src/Loops/Activity6.java
+++ src/Loops/Activity6.java
@@ -4,15 +4,17 @@ public class Activity6 {
 public static void main(String[]args){
 int number=0;
 int counter=0;
+int sum=0;
 int average=0;
 Scanner input=new Scanner(System.in);
 System.out.println("Enter A Number. (999 To Quit)");
 number=input.nextInt();
 while(number!=999)
 {
-average=number*8;
 System.out.println("Enter A Number. (999 To Quit)");
 counter++;
+sum+=number;
+average=sum/counter;
 number=input.nextInt();
 }
 System.out.println("The Average Of Numbers Entered Is: "+average);
--- src/Loops/Divisors.java
+++ src/Loops/Divisors.java
@@ -1,6 +1,6 @@
 package Loops;
 public class Divisors {
 public static void main(String[] args) {
-System.out.println("512");
+System.out.println("840");
 }
 }
