49a8342859434ccad71f6ed301a5f6a05b3329b0
Moneer Almawaldi <22almawm@elmbrookstudents.org>
2018-11-01 21:40:47 -0500

    HardStuff
    I think I will do well on tomorrow's test

---
+++ src/Loops/SumEven.java
@@ -0,0 +1,18 @@
+package Loops;
+import java.util.Scanner;
+public class SumEven {
+public static void main(String[] args) {
+Scanner input=new Scanner(System.in);
+int number=0;
+int counter=0;
+int answer=0;
+int amount=1000;
+for(counter=1;counter<=1;counter++)
+{
+System.out.println("Enter A Positive Three Digit Integer");
+number=input.nextInt();
+amount=1000-number/2;
+System.out.println("Answer: "+answer);
+}
+}
+}
